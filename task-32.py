ca93d7fc11b16b815fb76b662430811378add728
Sergey <aseev-1977@list.ru>
2023-03-31 18:26:48 +0300

    task-30

---
+++ task-32.py
@@ -0,0 +1,20 @@
+""" Задача 32: Определить индексы элементов массива (списка),
+значения которых принадлежат заданному диапазону (т.е. не
+меньше заданного минимума и не больше заданного
+максимума) 
+Ввод: [-5, 9, 0, 3, -1, -2, 1,
+4, -2, 10, 2, 0, -9, 8, 10, -9,
+0, -5, -5, 7]
+Вывод: [1, 9, 13, 14, 19] """
+
+def find_indexes(lst, min_val, max_val):
+    indexes = []
+    for i in range(len(lst)):
+        if min_val <= lst[i] <= max_val:
+            indexes.append(i)
+    return indexes
+
+arr = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+min_val = int(input())
+max_val = int(input())
+print(find_indexes(arr, min_val, max_val)) 
